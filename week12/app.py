b0198ea09c10eec7f6fbca907e62242dfa907757
Yakub Junaid <Junaidyakub28@gmail.com>
2024-12-15 12:20:16 +0100

    updated deprecated function

--- week12/app.py
+++ week12/app.py
@@ -33,7 +33,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display Uploaded Image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Get Image Class"):
         # Preprocess the image
